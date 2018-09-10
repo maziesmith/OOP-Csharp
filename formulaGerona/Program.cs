1e7fb3652457cfa7a431626b8efaac7ac5772f7c
Roman Onofreichuk <onofreichukroman@gmail.com>
2018-09-10 16:53:33 +0300

    first commit

--- formulaGerona/Program.cs
+++ formulaGerona/Program.cs
@@ -10,6 +10,20 @@ namespace formulaGerona
     {
         static void Main(string[] args)
         {
+            double a, b, c, p, s;
+            Console.WriteLine("Введите сторону a");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону b");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону c");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            p = (a + b + c) / 2;
+            s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine("По формуле Герона площадь треугольника S = {0}", s);
         }
     }
 }
